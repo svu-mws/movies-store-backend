84210d2c9fccf6578310b213d71658d8a4cae5a0
mohammadnashouq <mohammadnashouq@gmail.com>
2019-06-03 13:27:48 +0300

    Add files via upload

---
+++ ss.blade.php
@@ -0,0 +1,171 @@
+<div class="form-group row">
+    <label for="date" class="col-md-4 col-form-label text-md-right">{{ __('Birth day') }}</label>
+
+    <div class="col-md-6">
+        <input id="password-confirm" type="date" class="form-control" name="Birth_day">
+    </div>
+</div>
+
+<div class="form-group row">
+    <label for="age" class="col-md-4 col-form-label text-md-right">{{ __('age') }}</label>
+
+    <div class="col-md-6">
+        <input id="age" type="number" class="form-control" name="age" required>
+    </div>
+</div>
+
+<div class="form-group row">
+    <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Education_level') }}</label>
+
+    <div class="col-md-6">
+        <input id="password-confirm" type="text" class="form-control" name="password_confirmation" required autocomplete="new-password">
+    </div>
+</div>
+
+<div class="form-group row">
+    <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('gender') }}</label>
+
+    <div class="col-md-6">
+        <input id="password-confirm" type="text" class="form-control" name="password_confirmation" required autocomplete="new-password">
+    </div>
+</div>
+
+<div class="form-group row">
+    <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('homeonership') }}</label>
+
+    <div class="col-md-6">
+        <input id="password-confirm" type="text" class="form-control" name="password_confirmation" required autocomplete="new-password">
+    </div>
+</div>
+
+<div class="form-group row">
+    <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('internetconnection') }}</label>
+
+    <div class="col-md-6">
+        <input id="password-confirm" type="text" class="form-control" name="password_confirmation" required autocomplete="new-password">
+    </div>
+</div>
+
+<div class="form-group row">
+    <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('material_status') }}</label>
+
+    <div class="col-md-6">
+        <input id="password-confirm" type="text" class="form-control" name="password_confirmation" required autocomplete="new-password">
+    </div>
+</div>
+
+
+<div class="form-group row">
+  <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('movie_selector') }}</label>
+
+  <div class="col-md-6">
+      <input id="password-confirm" type="text" class="form-control" name="password_confirmation" required autocomplete="new-password">
+  </div>
+</div>
+
+
+<div class="form-group row">
+<label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('num_bathrooms') }}</label>
+
+<div class="col-md-6">
+    <input id="password-confirm" type="text" class="form-control" name="password_confirmation" required autocomplete="new-password">
+</div>
+</div>
+
+
+<div class="form-group row">
+<label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('num_bedrooms') }}</label>
+
+<div class="col-md-6">
+  <input id="password-confirm" type="text" class="form-control" name="password_confirmation" required autocomplete="new-password">
+</div>
+</div>
+
+
+<div class="form-group row">
+<label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('num_cars') }}</label>
+
+<div class="col-md-6">
+<input id="password-confirm" type="text" class="form-control" name="password_confirmation" required autocomplete="new-password">
+</div>
+</div>
+
+
+<div class="form-group row">
+<label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('num_cheildren') }}</label>
+
+<div class="col-md-6">
+<input id="password-confirm" type="text" class="form-control" name="password_confirmation" required autocomplete="new-password">
+</div>
+</div>
+
+<div class="form-group row">
+<label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('num_Tvs') }}</label>
+
+<div class="col-md-6">
+  <input id="password-confirm" type="text" class="form-control" name="password_confirmation" required autocomplete="new-password">
+</div>
+</div>
+<div class="form-group row">
+  <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('PPV_freq') }}</label>
+
+  <div class="col-md-6">
+      <input id="password-confirm" type="text" class="form-control" name="password_confirmation" required autocomplete="new-password">
+  </div>
+</div>
+  <div class="form-group row">
+      <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('buying_freq') }}</label>
+
+      <div class="col-md-6">
+          <input id="password-confirm" type="text" class="form-control" name="password_confirmation" required autocomplete="new-password">
+      </div>
+      </div>
+      <div class="form-group row">
+          <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('format') }}</label>
+
+          <div class="col-md-6">
+              <input id="password-confirm" type="text" class="form-control" name="password_confirmation" required autocomplete="new-password">
+          </div>
+</div>
+          <div class="form-group row">
+              <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('renting_freq') }}</label>
+
+              <div class="col-md-6">
+                  <input id="password-confirm" type="text" class="form-control" name="password_confirmation" required autocomplete="new-password">
+              </div>
+              </div>
+              <div class="form-group row">
+                  <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Viewing_freq') }}</label>
+
+                  <div class="col-md-6">
+                      <input id="password-confirm" type="text" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                  </div>
+                  </div>
+                  <div class="form-group row">
+                      <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Threater_freq') }}</label>
+
+                      <div class="col-md-6">
+                          <input id="password-confirm" type="text" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                      </div>
+                      </div>
+                      <div class="form-group row">
+                          <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('renting_freq') }}</label>
+
+                          <div class="col-md-6">
+                              <input id="password-confirm" type="text" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                          </div>
+                          </div>
+                          <div class="form-group row">
+                              <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Tv_movie_freq') }}</label>
+
+                              <div class="col-md-6">
+                                  <input id="password-confirm" type="text" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                              </div>
+                              </div>
+                              <div class="form-group row">
+                                  <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Tv_signal') }}</label>
+
+                                  <div class="col-md-6">
+                                      <input id="password-confirm" type="text" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                                  </div>
+                              </div>
